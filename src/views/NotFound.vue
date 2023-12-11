1375e2ca5dace98853ad2a9aa60e6cae53e1c46b
ishola10 <akoredeishola10@gmail.com>
2023-12-11 09:38:31 -0700

    'error'

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,35 @@
-<template>
+<!-- <template>
   <div>NotFound....</div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped></style> -->
+
+
+<template>
+  <div class="flex items-center justify-center bg-[url('../assets/images/login-bg.png')] h-screen bg-cover">
+    <div class="text-center">
+      <img
+        src="../assets/images/error.png"
+        alt="404 Image"
+        class="mx-auto mb-8 max-w-xs md:max-w-lg"
+      />
+      <h1 class="text-4xl md:text-6xl font-bold text-gray-800 mb-4">
+        &#128531;Oops! Page not found.
+      </h1>
+      <p class="text-xl md:text-2xl text-white mb-8">
+        Looks like you're lost in space.
+      </p>
+      <router-link
+        to="/"
+        class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md transition duration-300"
+        >Back to our site >></router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
