0e04e1c78a92a850dd2edf70c57d18a15e610847
oreolion <remyoreo11@gmail.com>
2024-01-20 07:51:58 -0800

    add: no-feedback component;

---
+++ src/components/no-feedback.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="max-w-4xl mx-auto my-10">
+    <div class="flex justify-center flex-col border h-[175px] w-[780px] pl-10">
+      <h3 class="text-3xl leading-10 font-bold mb-2">
+        No feedback to display 🔮
+      </h3>
+      <p class="w-[478px] text-base">
+        There is no feedback to display at this time – check back in a bit!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
