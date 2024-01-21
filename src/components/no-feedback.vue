365a77e2ce8808b17082e5be2a4f178f6f0b6664
oreolion <remyoreo11@gmail.com>
2024-01-21 03:26:14 -0800

    add: mobile nagivation menu component to navbar

--- src/components/no-feedback.vue
+++ src/components/no-feedback.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-4xl mx-auto my-10">
-    <div class="flex justify-center flex-col border h-[175px] w-[780px] pl-10">
+    <div class="flex justify-center flex-col border h-[175px] w-[90%] pl-10">
       <h3 class="text-3xl leading-10 font-bold mb-2">
         No feedback to display 🔮
       </h3>
